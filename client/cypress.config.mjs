b4fb36403c65fb05f58c2c1f18a283fa6c0fd5e8
Stef <polatsik@gmail.com>
2024-12-20 21:24:17 +0100

    e2e

--- client/cypress.config.mjs
+++ client/cypress.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'cypress'
 
+import codeCoverageTask from '@cypress/code-coverage/task.js'
+
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173'
-  }
+  },
+  setupNodeEvents(on, config) {
+    codeCoverageTask(on, config);
+    return config;
+  },
 })
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: ['.js', '.ts', '.vue'],
+      forceBuildInstrument: true
+      //requireEnv: true
+    }),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,5 +24,8 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:3000'
     }
+  },
+  build: {
+    sourcemap: true
   }
 })
